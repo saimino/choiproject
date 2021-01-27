6edbda102f789da599b0f958895ab6816c67c1d6
saimino <mino@choeminhoui-MacBookAir.local>
2021-01-27 20:39:34 +0900

    push

---
+++ 123plus2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+	int a,n,count,sum=0,i,j;
+	scanf("%d",&a);
+	for(i=0;i<a;i++){
+		for(j=2;j<4;j++){
+			if(sum+i==a){
+			sum=+i;
+			}
+			else
+			sum++;
+		}
+	}
+
+
+	printf("%d",count);
+
+
+
+
+
+
+
+
+}
